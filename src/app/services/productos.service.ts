f058e20283d0605afebbac599a0f93b5606272ff
Bladimir Gerson Parra Bermudez <ipproyectossoluciones@gmail.com>
2018-07-11 19:35:10 -0500

    Se agregan imagenes y servicio productos

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http } from "@angular/http";
+
+@Injectable()
+export class ProductosService {
+
+  productos:any[] = [];
+  cargando_productos = false;
+
+  constructor( private http:Http ) { 
+    this.cargar_productos ();
+  }
+  
+  public cargar_productos (){
+    this.cargando_productos = true;
+    if (this.productos.length === 0) {
+      this.http.get('https://portafolioweb-a486f.firebaseio.com/productos_idx.json')
+          .subscribe ( res => {
+            console.log( res.json() );
+            this.cargando_productos = false;
+          })
+    }
+  }
+}
